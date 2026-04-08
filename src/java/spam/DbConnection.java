4668e6f87eabb0357fd0bdbc384564fe5458f314
manmathanm52 <manmathanm52@example.com>
2026-04-08 22:44:32 +0530

    Configured for Cloud Deployment and harmonized connection utility

---
+++ src/java/spam/DbConnection.java
@@ -0,0 +1,15 @@
+package spam;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://mysql-240cfbf7-spamsql.i.aivencloud.com:21585/defaultdb?useSSL=true&allowPublicKeyRetrieval=true";
+        String user = "avnadmin";
+        String password = System.getenv("DB_PASSWORD");
+        return DriverManager.getConnection(url, user, password);
+    }
+}
