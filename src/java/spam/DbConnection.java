6322af9a5f920821940ad9557b330bfd57d74d26
manmathanm52 <manmathanm52@example.com>
2026-04-09 00:18:50 +0530

    Update database connection to use 'spam' database

--- src/java/spam/DbConnection.java
+++ src/java/spam/DbConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DbConnection {
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        String url = "jdbc:mysql://mysql-240cfbf7-spamsql.i.aivencloud.com:21585/defaultdb?useSSL=true&allowPublicKeyRetrieval=true";
+        String url = "jdbc:mysql://mysql-240cfbf7-spamsql.i.aivencloud.com:21585/spam?useSSL=true&allowPublicKeyRetrieval=true";
         String user = "avnadmin";
         String password = System.getenv("DB_PASSWORD");
         return DriverManager.getConnection(url, user, password);
